0cf0714b18031406f03d42a352218a2b64bfabab
l337 <autonomousmachine@gmail.com>
2014-12-18 11:10:15 -0800

    completed rankhospital added best and worst cases

--- rprogramming/assignment3/rankhospital.R
+++ rprogramming/assignment3/rankhospital.R
@@ -14,26 +14,21 @@ rankhospital <- function(state, outcome, num) {
     } else {
         subregion <- subset(data, data$State==state)
         subdata <- rank(subregion, outcome)
-        # sorted data from lowest to highest
-        newdata <- subdata[order(subdata$causeofdeath),]
-    }
-    
-    if(num == "best") {
+        newdata <- na.omit(subdata[order(subdata$causeofdeath, subdata$name),])
         
-    } else if(num == "worst") {
-        
-    } else {
-        firstvalue <- newdata[order(match(newdata$causeofdeath,newdata[num,"causeofdeath"])),]
+        # if num is outside the length of the data there shouldn't be any data for that result
+        if(is.numeric(num) & num > newdata$order[length(newdata)]) {
+            print(NA)
+        } else {
+            if(num == "best") {
+                as.character(newdata$name[1])
+            } else if(num == "worst") {
+                as.character(newdata$name[length(newdata$name)])
+            } else {
+                as.character(newdata[num,'name'])   
+            } 
+        }
     }
-    
-    # if num is outside the length of the data there shouldn't be any data for that result
-    if(num > newdata$order[length(newdata)]) {
-        print(NA)
-    } else {
-        # orders results based on match of num, returns the first result if there is a tie
-        as.character(firstvalue$name[1])   
-    }
-    ## 30-day death rate
 }
 
 # picks correct column based on outcome/state returns a dataframe with all apporiate data
@@ -45,5 +40,3 @@ rank <- function(hospital, columnresult) {
     values <- suppressWarnings(as.numeric(hospital[,outcome]))
     data.frame(order=1:length(values),name=hospital$Hospital.Name, state=hospital$State, causeofdeath=values)
 }
-
-rankhospital("TX", "heart failure", 15)
