21ccfc811d8119ab9592ff14cb79e0fb867cc30d
l337 <autonomousmachine@gmail.com>
2014-12-18 16:02:32 -0800

    raning all based on state if tie the hospital name in alphetical order is displayed first via aggregate function

--- rprogramming/assignment3/rankall.R
+++ rprogramming/assignment3/rankall.R
@@ -1,11 +1,22 @@
-rankall <- function(outcome, num = "best") {
+rankall <- function(outcome, num="best") {
     ## Read outcome data
-    outcome <- read.csv("outcome-of-care-measures.csv", colClasses="character")
-    
-    ## Check that state and outcome are valid
-    
+    data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
     ## For each state, find the hospital of the given rank
-    
-    ## Return a data frame with the hospital names and the
-    ## (abbreviated) state name
+    outcome <- switch(outcome, 
+                      "heart attack" = 11, 
+                      "heart failure" = 17,
+                      "pneumonia" = 23)
+    values <- suppressWarnings(as.numeric(data[,outcome]))
+    newdata <- data.frame(hospital=data$Hospital.Name, state=data$State, causeofdeath=values)
+    newerdata <- newdata[order(newdata$causeofdeath, newdata$hospital, na.last=NA),]
+    #newerdata <- split(newdata, newdata$state)
+    if(num == "best") {
+        result <- aggregate(newerdata, by = list(newerdata$state), function(x) head(x,1))
+    } else if(num == "worst") {
+        result <- aggregate(newerdata, by = list(newerdata$state), function(x) tail(x,1))
+    } else {
+        rank <- as.numeric(num)
+        result <- aggregate(newerdata, by = list(newerdata$state), function(x) x[rank])
+    }
+    data.frame(hospital=result$hospital, state=result$state)
 }
