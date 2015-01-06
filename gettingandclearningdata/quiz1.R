388dc43debbf23f7c8223a9b1a6b271142313bf5
l337 <autonomousmachine@gmail.com>
2015-01-06 14:31:49 -0800

    quiz one completed

--- gettingandclearningdata/quiz1.R
+++ gettingandclearningdata/quiz1.R
@@ -16,12 +16,10 @@ nrow(questionOne)
 # END Question One
 
 # Question Two is a tidy data question
-
 # Question Three: extracting data from Excel
 library(xlsx)
 fileExcelURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
 download.file(fileExcelURL, destfile="./data/naturalGasAquisitionProgram.xlsx", method="curl")
-
 dat <- read.xlsx("./data/naturalGasAquisitionProgram.xlsx", sheetIndex=1, colIndex=7:15, rowIndex=18:23)
 sum(dat$Zip*dat$Ext, na.rm=T)
 # END Question Three
@@ -37,7 +35,18 @@ zipcodes <- xpathSApply(rootNode, "//zipcode", xmlValue)
 sum(zipcodes == 21231)
 # END Question Four
 
-# Question Five
+# Question Five: testing the fastest algorithm using fread (faster than read.table)
+library(data.table)
+fileCsvURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileCsvURL, destfile="./data/newidahohousing.csv", method="curl")
 
+temp <- fread("./data/newidahohousing.csv")
+DT <- data.table(temp)
 
+# Different ways to calculate the average value
+system.time(mean(DT[DT$SEX==1,]$pwgtp15))
+system.time(mean(DT$pwgtp15,by=DT$SEX))
+system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
+system.time(DT[,mean(pwgtp15),by=SEX]) #***
+system.time(tapply(DT$pwgtp15,DT$SEX,mean))
 # END Question Five
