1f5cadb9c2f7d9f1dcdf485b6ff2f05bb58de3b6
l337 <autonomousmachine@gmail.com>
2015-01-06 13:23:57 -0800

    loading xml and excel sheets into R, and formatting the data appropriately

--- gettingandclearningdata/quiz1.R
+++ gettingandclearningdata/quiz1.R
@@ -1,17 +1,43 @@
-# Question One
+# Question One: extracting data from CSV
 if(!file.exists("data")) { dir.create("data") }
-fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-download.file(fileURL, destfile="./data/idahohousing.csv", method="curl")
+
+library(dplyr)
+fileCsvURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileCsvURL, destfile="./data/idahohousing.csv", method="curl")
 
 dateDownloaded <- date()
 dateDownloaded
 
-library(dplyr)
 housingData <- read.csv("./data/idahohousing.csv")
 data <- tbl_df(housingData)
 propertyValues <- select(data, !is.na(VAL))
-questionOne <- filter(propertyValues, VAL >= 24) #24. 1,000,000+ Net Income on house
+questionOne <- filter(propertyValues, VAL >= 24) #24. 1,000,000+ Net Value on house
 nrow(questionOne)
+# END Question One
 
-file2URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
-download.file(file2URL, destfile="./data/naturalGasAquisitionProgram.xlsx", method="curl")
+# Question Two is a tidy data question
+
+# Question Three: extracting data from Excel
+library(xlsx)
+fileExcelURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileExcelURL, destfile="./data/naturalGasAquisitionProgram.xlsx", method="curl")
+
+dat <- read.xlsx("./data/naturalGasAquisitionProgram.xlsx", sheetIndex=1, colIndex=7:15, rowIndex=18:23)
+sum(dat$Zip*dat$Ext, na.rm=T)
+# END Question Three
+
+# Question Four: extracting data from XML
+library(RCurl)
+library(XML)
+# bypassing https, instead of removing the s:
+fileXmlURL <- getURL("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml", ssl.verifypeer = FALSE)
+doc <- xmlTreeParse(fileXmlURL, useInternal=T)
+rootNode <- xmlRoot(doc)
+zipcodes <- xpathSApply(rootNode, "//zipcode", xmlValue)
+sum(zipcodes == 21231)
+# END Question Four
+
+# Question Five
+
+
+# END Question Five
