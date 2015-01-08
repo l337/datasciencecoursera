9aa0198a9b4051eb9faeb4a9073baf218cc6caa9
l337 <autonomousmachine@gmail.com>
2015-01-08 11:18:27 -0800

    completed github api

--- gettingandclearningdata/quiz2.R
+++ gettingandclearningdata/quiz2.R
@@ -1,9 +1,11 @@
 # Question one: using github api to retrieve repo json
 library(httr)
+library(jsonlite)
 
 # hidden values, not visible for obvious reasons
-apikey <- "<api key from github app>"
-secret <- "<api secret from github app>"
+apikey <- "<20 character length hidden key>"
+secret <- "<40 character length hidden secret>"
+repoName <- "datasharing"
 
 # required for github api use
 github <- oauth_endpoints("github")
@@ -11,10 +13,19 @@ myapp <- oauth_app("github", key=apikey, secret=secret)
 github_token <- oauth2.0_token(github, myapp)
 
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/rate_limit", gtoken)
-stop_for_status(req)
-content(req)
-
-# fromJSON(toJSON(messy_stuff))
-
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+# stop_for_status(req)
+# content(req)
+jsonObject <- fromJSON(toJSON(content(req)))
+createdAt <- jsonObject$created_at[5]
+createdAt
 # END Question one
+
+# Question two
+fileCsvURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileCsvURL, destfile="./data/americancommunitysurvey", method="curl")
+data <- read.csv("./data/idahohousing.csv")
+data
+
+
+# END Question two
