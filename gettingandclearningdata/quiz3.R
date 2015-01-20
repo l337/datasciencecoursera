657ccf48d2617dc3a08894838becea6599d48c03
l337 <autonomousmachine@gmail.com>
2015-01-20 11:01:18 -0800

    finished week 3 quiz starting problem set

--- gettingandclearningdata/quiz3.R
+++ gettingandclearningdata/quiz3.R
@@ -1,4 +1,4 @@
-# Question One
+# Question one
 library(dplyr)
 fileCsvURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 download.file(fileCsvURL, destfile="./data/idahohousing.csv", method="curl")
@@ -6,26 +6,49 @@ data <- read.csv("./data/idahohousing.csv")
 selectdata <- select(data, ACR, AGS)
 agricultureLogical <- selectdata[which(selectdata$ACR == 3 & selectdata$AGS == 6),]
 agricultureLogical
+# END Question one
 
-# Question Two
+# Question two
 library(jpeg)
 filejpgURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
 download.file(filejpgURL, destfile="./data/data.jpg", method="curl")
 img.n <- readJPEG("./data/data.jpg", native=TRUE)
 quantile(img.n, c(0.3, 0.8))
+# END Question two
 
-# Question Three
+# Question three
+library(plyr)
 grossdomestic <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 educational <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
 download.file(grossdomestic, destfile="./data/grossdomestic.csv", method="curl")
 download.file(educational, destfile="./data/educational.csv", method="curl")
 
-domesticgrossdata <- read.csv("./data/grossdomestic.csv")
-educationaldata <- read.csv("./data/educational.csv")
+gdpdata <- read.csv("./data/grossdomestic.csv", skip=5, nrows=190, stringsAsFactors = F)
+edudata <- read.csv("./data/educational.csv", stringsAsFactors = FALSE)
 
-mergedData <- merge(domesticgrossdata, educationaldata, by.x="X", by.y="CountryCode")
-sorteddata <- arrange(mergedData, desc(Gross.domestic.product.2012))
-sorteddata[13,]
+gdpdata <- gdpdata[,c(1, 2, 4, 5)]
+colnames(gdpdata) <- c("CountryCode", "Rank", "Country.Name", "GDP.Value")
 
-# str(domesticgrossdata)
-# names(educationaldata)
+mergedData <- merge(gdpdata, edudata, by.x="CountryCode", by.y="CountryCode", all=F)
+sum(!is.na(unique(mergedData$Rank)))
+arrange(mergedData, desc(Rank))[13, 3]
+# END Qestion three
+
+# Question four
+# high income OECD based on GDP rank
+oecd <- subset(mergedData, Income.Group %in% "High income: OECD", select = c(Rank))
+mean(oecd$Rank)
+
+# high icome nonOECD based on GDP rank
+nonoecd <- subset(mergedData, Income.Group %in% "High income: nonOECD", select = c(Rank))
+mean(nonoecd$Rank)
+# END Question four
+
+# Question five
+mergedData[, mean(Rank, na.rm = TRUE), by = Income.Group]
+
+breaks <- quantile(dt$rankingGDP, probs = seq(0, 1, 0.2), na.rm = TRUE)
+mergedData$quantileGDP <- cut(dt$rankingGDP, breaks = breaks)
+mergedData[Income.Group == "Lower middle income", .N, by = c("Income.Group", "quantileGDP")]
+
+
