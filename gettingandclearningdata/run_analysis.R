d7775cab8f713c7b1f3690d17cc1976c3f3647fb
l337 <autonomousmachine@gmail.com>
2015-01-20 12:15:16 -0800

    start on the class assignment

---
+++ gettingandclearningdata/run_analysis.R
@@ -0,0 +1,17 @@
+# 1) Merge the training and the test sets to create one data set.
+# 2) Extract only the measurements on the mean and standard deviation for each measurement. 
+# 3) Use descriptive activity names to name the activities in the data set
+# 4) Appropriately label the data set with descriptive activity names. 
+# 5) Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+activityType <- read.table('./data/UCI HAR Dataset/activity_labels.txt')
+subjectTrain <- read.table('./data/UCI HAR Dataset/train/subject_train.txt')
+xTrain <- read.table('./data/UCI HAR Dataset/train/x_train.txt')
+yTrain <- read.table('./data/UCI HAR Dataset/train/y_train.txt')
+
+# Assigin column names to the data imported above
+colnames(activityType) <- c('activityId','activityType');
+colnames(subjectTrain) <- "subjectId";
+colnames(xTrain) <- features[,2]; 
+colnames(yTrain) <- "activityId";
